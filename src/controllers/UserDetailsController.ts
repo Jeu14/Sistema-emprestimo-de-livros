b9d92a13bbef9f4da2c9b133063028a7f6e4eaca
DESKTOP-H9UVA2A\User <jedissonsantana34@gmail.com>
2024-03-27 18:18:10 -0300

    new user d

--- src/controllers/UserDetailsController.ts
+++ src/controllers/UserDetailsController.ts
@@ -1,26 +1,15 @@
 import { Request, Response } from "express";
-import { userRepository } from "../repositories/userRepository";
 import { loggedUser } from "./LoggedUser";
 import { IUserData } from "../interfaces/IUserData";
 
 export class userDetails {
     async index(req: Request, res: Response) {
-        const userId: IUserData = await loggedUser(req);
-
-        if (!userId) {
-            return res.json({ mensage: "ruim" });
-        }
-
         try {
-            const user = await userRepository.findOne({
-                where: { id: userId.id },
-            });
-
-            const { senha, ...userData } = user!;
-            return res.status(200).json(userData);
+            const userId: IUserData = await loggedUser(req);
+            return res.status(200).json(userId);
         } catch (error) {
             res.status(500).json({
-                message: "Não foi possível cadastrar o usuário",
+                message: "Não foi possível detalhar as informações do usuário",
             });
         }
     }
