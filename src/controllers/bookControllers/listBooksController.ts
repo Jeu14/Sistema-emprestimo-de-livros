f9d0b41c70d8467060c266c729df18964b90b01f
DESKTOP-H9UVA2A\User <jedissonsantana34@gmail.com>
2024-04-02 03:14:49 -0300

    status code added on listBooksController

--- src/controllers/bookControllers/listBooksController.ts
+++ src/controllers/bookControllers/listBooksController.ts
@@ -5,7 +5,7 @@ export class listBooks {
     async list(req: Request, res: Response) {
         try {
             const books = await bookRepository.find();
-            res.json(books);
+            res.status(200).json(books);
         } catch (error) {
             res.status(500).json({
                 mensagem: "Não foi possível efetuar a listagem de livros",
