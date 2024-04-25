684a6dd1d46950b7d1827dc12f8b888f1e07cc87
Jeu14 <jedissonsantana34@gmail.com>
2024-04-24 22:07:25 -0300

    bug: Request and Response causing conflict on some routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import { registerUserController } from "./controllers/userControllers/RegisterUserController";
 import { userLoginController } from "./controllers/userControllers/UserLoginController";
 import { userDetails } from "./controllers/userControllers/UserDetailsController";
